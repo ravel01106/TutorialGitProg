46f449d8741c2c5c8e4380c2ab5d2ba28f1b2de7
AmandaRaveloCabrera <ravello2002@gmail.com>
2023-03-28 10:52:22 +0100

    feat(main): prueba

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo Cruel");
         final Scanner KEYBOARD = new Scanner(System.in);
         System.out.println("Introduce un número:");
         String number = KEYBOARD.nextLine();
