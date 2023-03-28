dbb78fcc56159ec9cfecb6928185a5e2eeda43d9
AmandaRaveloCabrera <usr3573@salesianos-lacuesta.net>
2023-03-28 10:43:20 +0100

    feat(Main): add user number input to sout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo Cruel");
+        final Scanner KEYBOARD = new Scanner(System.in);
+        System.out.println("Introduce un número:");
+        String number = KEYBOARD.nextLine();
+        System.out.println("Hola Mundo " + number + "!");
+        KEYBOARD.close();
 
     }
 }
